ac3927f2d69f31841dade7b12392df1ae044ac9c
Jashann <48613250+Jashann@users.noreply.github.com>
2020-09-13 10:30:28 +0530

    Updates

---
+++ precache-manifest.2436dac38adcf320498e93ef2593d1d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dca602824440f870f85205a6cd0d637",
+    "url": "/React_Redux_Routing/index.html"
+  },
+  {
+    "revision": "7f32bc3ace4c7f0e9b66",
+    "url": "/React_Redux_Routing/static/css/main.2a9c0afa.chunk.css"
+  },
+  {
+    "revision": "ea8276f755eacbe908de",
+    "url": "/React_Redux_Routing/static/js/2.6ad51afd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Redux_Routing/static/js/2.6ad51afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f32bc3ace4c7f0e9b66",
+    "url": "/React_Redux_Routing/static/js/main.48b5e1ee.chunk.js"
+  },
+  {
+    "revision": "d4378a76019201d88328",
+    "url": "/React_Redux_Routing/static/js/runtime-main.54793d4c.js"
+  }
+]);
