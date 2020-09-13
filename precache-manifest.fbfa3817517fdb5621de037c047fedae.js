d643099c7924c34a2ddf7fbee07056c0d68e2763
Jashann <48613250+Jashann@users.noreply.github.com>
2020-09-13 10:35:43 +0530

    Updates

--- precache-manifest.fbfa3817517fdb5621de037c047fedae.js
+++ precache-manifest.fbfa3817517fdb5621de037c047fedae.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "72f47282db6c15e8ba0f492d7dcef3d0",
+    "revision": "8816a22f70859cc0a5d5731e48925cba",
     "url": "/React_Redux_Routing/index.html"
   },
   {
-    "revision": "b7554df8513d57215e08",
+    "revision": "9f9b7fe0822c61f22dd1",
     "url": "/React_Redux_Routing/static/css/main.2a9c0afa.chunk.css"
   },
   {
-    "revision": "b0e03a65e371193b11e2",
-    "url": "/React_Redux_Routing/static/js/2.d4dd6b71.chunk.js"
+    "revision": "de1ff57ae8fa4ba546d1",
+    "url": "/React_Redux_Routing/static/js/2.17a3368f.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/React_Redux_Routing/static/js/2.d4dd6b71.chunk.js.LICENSE.txt"
+    "url": "/React_Redux_Routing/static/js/2.17a3368f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b7554df8513d57215e08",
-    "url": "/React_Redux_Routing/static/js/main.d9f6812e.chunk.js"
+    "revision": "9f9b7fe0822c61f22dd1",
+    "url": "/React_Redux_Routing/static/js/main.95284ae1.chunk.js"
   },
   {
     "revision": "d4378a76019201d88328",
